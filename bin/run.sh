f4cb60419f5dd4d67302eb37c5c91081cbebddf2
Emmanuel Marboeuf <emmanuel@visage.ae>
2017-04-11 19:21:32 -0700

    removed wrong addr

--- bin/run.sh
+++ bin/run.sh
@@ -2,7 +2,7 @@
 
 mongoDRDLParams="--host $MONGO_HOST --port $MONGO_PORT -d $DB_NAME -o schema.drdl"
 MONGO_URI="mongodb://${MONGO_HOST}:${MONGO_PORT}"
-mongoSQLDParams="--addr mongo-bi-connector:3307 --schema schema.drdl --mongo-uri $MONGO_URI"
+mongoSQLDParams="--schema schema.drdl --mongo-uri $MONGO_URI"
 if [ -n "$MONGO_USERNAME" ] && [ -n "$MONGO_PASSWORD" ]  && [ -n "$MONGO_AUTH_DB" ]  && [ -n "$SSL_STRING" ]; then
     if [ ! -f ./bi_connector_auto_signed.pem ]; then
         openssl req -nodes -newkey rsa:2048 -keyout temp.key -out temp.crt -x509 -days 365 -subj $SSL_STRING
