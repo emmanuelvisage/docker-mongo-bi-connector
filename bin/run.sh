b2106aabe4a029585f9e6d34b1107bab5e9a7ebf
Emmanuel Marboeuf <emmanuel@visage.ae>
2017-08-03 01:35:30 +0200

    new mongo bi connector version

--- bin/run.sh
+++ bin/run.sh
@@ -9,17 +9,13 @@ fi
 
 mongoDRDLParams="--host $MONGO_HOST --port $MONGO_PORT -d $DB_NAME -o schema.drdl"
 MONGO_URI="mongodb://${MONGO_HOST}:${MONGO_PORT}"
-mongoSQLDParams="--schema ${schemaFile} --mongo-uri $MONGO_URI"
+mongoSQLDParams="--addr mongo-bi-connector:3307 --schema ${schemaFile} --mongo-uri $MONGO_URI"
 
-if [ -n "$MONGO_USERNAME" ] && [ -n "$MONGO_PASSWORD" ]  && [ -n "$MONGO_AUTH_DB" ]  && [ -n "$SSL_STRING" ]; then
-    if [ ! -f ./bi_connector_auto_signed.pem ]; then
-        openssl req -nodes -newkey rsa:2048 -keyout temp.key -out temp.crt -x509 -days 365 -subj $SSL_STRING
-        cat temp.crt temp.key > bi_connector_auto_signed.pem
-    fi
+if [ -n "$MONGO_USERNAME" ] && [ -n "$MONGO_PASSWORD" ]  && [ -n "$MONGO_AUTH_DB" ]; then
     USER_STRING="${MONGO_USERNAME}:${MONGO_PASSWORD}@"
     MONGO_URI="mongodb://${USER_STRING}${MONGO_HOST}:${MONGO_PORT}"
     mongoDRDLParams="${mongoDRDLParams} --username ${MONGO_USERNAME} --password ${MONGO_PASSWORD} --authenticationDatabase ${MONGO_AUTH_DB} "
-    mongoSQLDParams="${mongoDRDLParams} --auth --sslPEMKeyFile bi_connector_auto_signed.pem"
+    mongoSQLDParams="${mongoSQLDParams} --auth"
 fi
 
 TEMP_FILE="/tmp/mysql.sock";
