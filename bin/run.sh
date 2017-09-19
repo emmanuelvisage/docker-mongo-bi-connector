bf39ca8515111d998bfd1004f9154ced66b06387
Emmanuel Marboeuf <emmanuel@visage.ae>
2017-09-19 13:34:07 -0700

    now accepts mongo URI

--- bin/run.sh
+++ bin/run.sh
@@ -1,28 +1,44 @@
 #!/bin/sh
 
 rm -f tmpschema.drdl
-schemaFile="schema.drdl"
-if [ "$1" = "-e" -a input="-" ];then
+if [ -n "$1" ] && [ "$1" = "-e" -a input="-" ];then
     cat $input >> tmpschema.drdl
     schemaFile="tmpschema.drdl"
 fi
+if [ -n "$SCHEMA_PATH" ]; then
+    schemaFile=$SCHEMA_PATH
+fi
 
-mongoDRDLParams="--host $MONGO_HOST --port $MONGO_PORT -d $DB_NAME -o schema.drdl"
-MONGO_URI="mongodb://${MONGO_HOST}:${MONGO_PORT}"
+echo $schemaFile
+
+#if [ -z "$schemaFile" ]; then
+#    mongoDRDLParams="--host $MONGO_HOST --port $MONGO_PORT -d $DB_NAME -o schema.drdl"
+#    if [ -n "$MONGO_USERNAME" ] && [ -n "$MONGO_PASSWORD" ]  && [ -n "$MONGO_AUTH_DB" ]; then
+#        mongoDRDLParams="${mongoDRDLParams} --username ${MONGO_USERNAME} --password ${MONGO_PASSWORD} --authenticationDatabase ${MONGO_AUTH_DB} "
+#    fi
+#    `/usr/local/bin/mongodrdl ${mongoDRDLParams}`
+#    schemaFile="schema.drdl"
+#fi
+
+if [ -n "$MONGO_HOST" ] && [ -n "$MONGO_PORT" ]; then
+    MONGO_URI="mongodb://${USER_STRING}${MONGO_HOST}:${MONGO_PORT}"
+fi
+
+echo $MONGO_URI
+
+
 mongoSQLDParams="--addr $ADDR --schema ${schemaFile} --mongo-uri $MONGO_URI"
 
-if [ -n "$MONGO_USERNAME" ] && [ -n "$MONGO_PASSWORD" ]  && [ -n "$MONGO_AUTH_DB" ]; then
-    USER_STRING="${MONGO_USERNAME}:${MONGO_PASSWORD}@"
-    MONGO_URI="mongodb://${USER_STRING}${MONGO_HOST}:${MONGO_PORT}"
-    mongoDRDLParams="${mongoDRDLParams} --username ${MONGO_USERNAME} --password ${MONGO_PASSWORD} --authenticationDatabase ${MONGO_AUTH_DB} "
-    mongoSQLDParams="${mongoSQLDParams} --auth"
+echo $mongoSQLDParams
+
+if [ -n "$MONGO_AUTH" ]; then
+    mongoSQLDParams="${mongoSQLDParams} --auth --mongo-ssl"
 fi
+
+echo $mongoSQLDParams
 
 TEMP_FILE="/tmp/mysql.sock";
 rm -f $TEMP_FILE
 
-if ! [ -f "tmpschema.drdl" ]; then
-    `/usr/local/bin/mongodrdl ${mongoDRDLParams}`
-fi
 
 `/usr/local/bin/mongosqld ${mongoSQLDParams}`
