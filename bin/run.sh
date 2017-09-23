397998521d3c9ea84c09f098415363d42bc750ec
Emmanuel Marboeuf <emmanuel@visage.ae>
2017-09-22 19:36:17 -0700

    re enable the schema generation when doesn t exist

--- bin/run.sh
+++ bin/run.sh
@@ -9,16 +9,14 @@ if [ -n "$SCHEMA_PATH" ]; then
     schemaFile=$SCHEMA_PATH
 fi
 
-echo $schemaFile
-
-#if [ -z "$schemaFile" ]; then
-#    mongoDRDLParams="--host $MONGO_HOST --port $MONGO_PORT -d $DB_NAME -o schema.drdl"
-#    if [ -n "$MONGO_USERNAME" ] && [ -n "$MONGO_PASSWORD" ]  && [ -n "$MONGO_AUTH_DB" ]; then
-#        mongoDRDLParams="${mongoDRDLParams} --username ${MONGO_USERNAME} --password ${MONGO_PASSWORD} --authenticationDatabase ${MONGO_AUTH_DB} "
-#    fi
-#    `/usr/local/bin/mongodrdl ${mongoDRDLParams}`
-#    schemaFile="schema.drdl"
-#fi
+if [ -z "$schemaFile" ]; then
+    mongoDRDLParams="--host $MONGO_HOST --port $MONGO_PORT -d $DB_NAME -o schema.drdl"
+    if [ -n "$MONGO_USERNAME" ] && [ -n "$MONGO_PASSWORD" ]  && [ -n "$MONGO_AUTH_DB" ]; then
+        mongoDRDLParams="${mongoDRDLParams} --username ${MONGO_USERNAME} --password ${MONGO_PASSWORD} --authenticationDatabase ${MONGO_AUTH_DB} "
+    fi
+    `/usr/local/bin/mongodrdl ${mongoDRDLParams}`
+    schemaFile="schema.drdl"
+fi
 
 if [ -n "$MONGO_HOST" ] && [ -n "$MONGO_PORT" ]; then
     MONGO_URI="mongodb://${USER_STRING}${MONGO_HOST}:${MONGO_PORT}"
@@ -28,8 +26,6 @@ echo $MONGO_URI
 
 
 mongoSQLDParams="--addr $ADDR --schema ${schemaFile} --mongo-uri $MONGO_URI"
-
-echo $mongoSQLDParams
 
 if [ -n "$MONGO_AUTH" ]; then
     mongoSQLDParams="${mongoSQLDParams} --auth --mongo-ssl"
